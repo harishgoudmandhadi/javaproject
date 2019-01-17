0401112a764babdf22c0107b10e7cf5bb4b18f66
Harish <harishgoudmandhadi@gmail.com>
2019-01-17 15:26:51 -0500

    fix test

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -6,7 +6,7 @@ package com.mycompany.app;
 public class App
 {
 
-    private final String message = "Hello World!i--testing CI build";
+    private final String message = "Hello World!--testing CI build";
 
     public App() {}
 
